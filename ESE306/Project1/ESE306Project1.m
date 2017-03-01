d4acf94c1132d0cf4a38bf6cfd9ed8e931677937
Edward Wang <edward.wang.95@gmail.com>
2017-03-01 17:50:43 -0500

    Setting up classes for matlab

---
+++ ESE306/Project1/ESE306Project1.m
@@ -0,0 +1,70 @@
+%**************************************************************************
+% Program name:     ESE306_Project1_Binomial.m
+% Author:           Edward Wang && Vineet Lachwani
+% Date Created:     February 27, 2017
+% Last Update:      February 27, 2017
+%
+% ESE 306 Project showing binomial,
+%
+%
+%
+%
+%**************************************************************************
+classdef ESE306Project1
+    properties
+    end
+    
+    methods(Static = true)
+        %Constructor
+        function obj = ESE306Project1()
+           
+        end
+        
+        function listDistributions()
+            fprintf('ALL DISTRIBUTIONS\n');
+            fprintf('1]Binomial\n');
+            fprintf('2]Poisson\n');
+            fprintf('3]Normal\n');
+            fprintf('4]Befa\n');
+            %fprintf('Poisson Distribution:\nPoisson(x,y)\nx = lamda\ny = number of trials\n');
+            %fprintf('Normal Distribution:\nNormal(x,y,z)\nx = mu\ny = sigma\nz = number of samples\n');
+            %fprintf('Beta Distribution:\nBeta(x,y,z)\nx = alpha\ny = beta\nz = number of samples\n');
+
+            
+           
+        end
+        
+        %Binomial
+        function binomial()
+            fprintf('Binomial Distribution:\n');
+            fprintf('Binomial(numOfTrials, prob, numOfSamples)\n');
+            prompt = 'Enter number of trials: ';
+            SampleSize = input(prompt);
+            numOfTrials = SampleSize;
+            prompt = 'Enter probability of each success: ';
+            prob = input(prompt);
+            prompt = 'Enter number of samples: ';
+            numOfSamples = input(prompt);
+            
+            %Get binomial array
+            binArray = binornd(numOfTrials, prob, [1, numOfSamples]);
+            histogram(binArray,numOfTrials);
+            %Compute the sample mean and variance
+            %avg = mean(binArray);
+                %variance = var(binArray);
+                %Moments utilizing the 'binostat' function
+            [avg, variance] = binostat(numOfTrials, prob);
+            % Compute the parameters of the distribution from data
+            param = binofit(binArray, numOfTrials);
+            %Display the results
+            fprintf('Binomial Distribution\n');
+            fprintf('Sample Mean: %f\n', avg);
+            fprintf('Sample Variance: %f\n', variance);
+            fprintf('Theoretical Mean: %f\n', avg);
+            fprintf('Theoretical Variance: %f\n', variance);
+                %fprintf('Distribution Parameters: %f\n', param);
+        end
+    end
+    
+end
+
--- Project1Will/Main.m
+++ Project1Will/Main.m
@@ -1,5 +1,33 @@
-fprintf('ALL DISTRIBUTIONS\n');
+
+%fprintf('Binomial Distribution:\nBinomial(x,y,z)\nx = number of trials\ny = probability\nz = number of samples\n');
+%fprintf('Poisson Distribution:\nPoisson(x,y)\nx = lamda\ny = number of trials\n');
+%fprintf('Normal Distribution:\nNormal(x,y,z)\nx = mu\ny = sigma\nz = number of samples\n');
+%fprintf('Beta Distribution:\nBeta(x,y,z)\nx = alpha\ny = beta\nz = number of samples\n');
+
+%Ask user for distribution
+%Create project
+project = ESE306Project1();
+while(1)
+    project.listDistributions();
+    prompt = ('Choice: ');
+    choice = input(prompt);
+    switch choice
+        case 0
+            break;
+        case 1
+            %Binomial
+            fprintf('Binomial');
+            break;
+    end
+end
+
+%Test Binomial
 fprintf('Binomial Distribution:\nBinomial(x,y,z)\nx = number of trials\ny = probability\nz = number of samples\n');
-fprintf('Poisson Distribution:\nPoisson(x,y)\nx = lamda\ny = number of trials\n');
-fprintf('Normal Distribution:\nNormal(x,y,z)\nx = mu\ny = sigma\nz = number of samples\n');
-fprintf('Beta Distribution:\nBeta(x,y,z)\nx = alpha\ny = beta\nz = number of samples\n');
+prompt = 'Enter number of trials: ';
+SampleSize = input(prompt);
+numOfTrials = SampleSize;
+prompt = 'Enter probability of each success: ';
+prob = input(prompt);
+prompt = 'Enter number of samples: ';
+numOfSamples = input(prompt);
+project.binomial(numOfTrials, prob, numOfSamples);
